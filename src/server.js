9c5df14c365091407da9a5b03c48b438b58402ed
jrb <jrb.youssef@gmail.com>
2020-02-15 21:43:23 +0100

    remove path, port and db from server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
-const path = require('path')
 const express = require('express')
 // const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 app.use(express.json())
 
 const Router = require('./routes')
 Router(app)
-
-const db = require('./db')
-
 
 
 app.listen(PORT, () => {
