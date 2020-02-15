5ccb01a88781810ff9f0c0b6aa25fe9307cdfe13
jrb <jrb.youssef@gmail.com>
2020-02-15 21:12:09 +0100

    commenting function

--- src/utils/Handler.js
+++ src/utils/Handler.js
@@ -1,3 +1,10 @@
+/**
+ * Because we can't try catch along with async await, we need to wrappe the 
+ * promise in another promise.
+ * 
+ * @date    15/02/2020
+ * @author  jrb <jrb.youssef@gmail.com>
+ */
 module.exports = (promise) => {
   return promise
     .then(data => ([data, undefined]))
